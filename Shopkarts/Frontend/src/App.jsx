497aa62057409d60df9ae30374f57337d374d1b3
sagarchaurasia176 <sagarchaurasia176@gmail.com>
2024-06-01 13:28:48 +0530

    code change in redux

--- Shopkarts/Frontend/src/App.jsx
+++ Shopkarts/Frontend/src/App.jsx
@@ -1,12 +1,21 @@
 import "./App.css";
+import CategoryNav from "./Screens/CategoryNav";
 import Navbar from "./Screens/Navbar";
+import MainProductPage from "./page/MainProductPage";
+import LocomotiveScroll from 'locomotive-scroll';
+
+const locomotiveScroll = new LocomotiveScroll();
 
 function App() {
   return (
     <>
       <div>
-        <div>
-          <Navbar />
+        <div className="h-screen">
+          <div className="">
+            <Navbar />
+            <CategoryNav />
+          </div>
+          <MainProductPage/>
         </div>
       </div>
     </>
